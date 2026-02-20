287804744b12912fcf52a446b3140c46edb45b63
HUMBERTOUNI <hmontenegrosa@gmail.com>
2026-02-19 23:09:02 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4 --timeout-keep-alive 120
+exec uvicorn api:app --host 0.0.0.0 --port 8080 --workers 4 --timeout-keep-alive 120
