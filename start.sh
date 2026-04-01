cef147070e5e3f1d25be34a0246a05a874a0a3dc
HUMBERTOUNI <hmontenegrosa@gmail.com>
2026-04-01 10:29:19 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sentinel Quantum Edge v5.3 - Optimized for Hetzner CX43 (8 CPU, 16GB RAM)
-exec uvicorn api:app --host 0.0.0.0 --port 9000 --workers 8 --timeout-keep-alive 180
+exec uvicorn api_11:app --host 0.0.0.0 --port 9000 --workers 8 --timeout-keep-alive 180
